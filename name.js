229d1ca9f458a809237d6ad6a90b0a8b5c01316f
Esmyx <valentin.sicotpoulain@gmail.com>
2024-07-13 17:28:15 +0200

    some changes LOOOL

---
+++ name.js
@@ -0,0 +1,28 @@
+var singers = [
+    "Olivia Rodrigo",
+    "Sabrina Carpenter",
+    "Benson Boone",
+    "Nessa Barrett",
+    "Tate McRae",
+    "Madison Beer",
+    "Joshua Bassett",
+    "Billie Eilish",
+    "Ariana Grande",
+    "Taylor Swift",
+    "Dua Lipa",
+    "Justin Bieber",
+    "Demi Lovato",
+    "Maroon 5",
+    "Reneé Rapp",
+    "Dove Cameron",
+]
+
+var actors = [
+    "Ariana Greenblatt",
+    "Drew Starkey",
+    "Jennifer Aniston",
+    "Sydney Sweeney",
+    "Madelyn Cline",
+    "Pierre Niney",
+    "Adèle Exarchopoulos",
+]
