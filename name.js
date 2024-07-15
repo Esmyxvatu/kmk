2963037d797e7a8255b175b0628914171ec32f20
Esmyx <valentin.sicotpoulain@gmail.com>
2024-07-15 15:03:40 +0200

    added the possibility to edit and create custom category

--- name.js
+++ name.js
@@ -84,95 +84,23 @@ var lists = {
         "Kendall Jenner",
         "Hailey Bieber",
     ],
-    all: [
-        "Olivia Rodrigo",
-        "Sabrina Carpenter",
-        "Benson Boone",
-        "Nessa Barrett",
-        "Tate McRae",
-        "Madison Beer",
-        "Joshua Bassett",
-        "Billie Eilish",
-        "Ariana Grande",
-        "Taylor Swift",
-        "Dua Lipa",
-        "Justin Bieber",
-        "Demi Lovato",
-        "Maroon 5",
-        "Reneé Rapp",
-        "Dove Cameron",
-        "Drex Carter",
-        "Avicii",
-        "Neffex",
-        "Yuston XIII",
-        "Orelsan",
-        "T3NZU",
-        "Imagine Dragons",
-        "OneRepublic",
-        "David Guetta",
-        "TheFatRat",
-        "Avril Lavigne",
-        "Celine Dion",
-        "Kurt Kobain",
-        "Rihanna",
-        "Emily Osment",
-        "Ethan Cutkosky",
-        "Ben Barnes",
-        "Conan Grey",
-        "Ariana Greenblatt",
-        "Drew Starkey",
-        "Jennifer Aniston",
-        "Sydney Sweeney",
-        "Madelyn Cline",
-        "Pierre Niney",
-        "Adèle Exarchopoulos",
-        "Timothée Chalamet",
-        "Alexandra Demi",
-        "Dylan O'brien",
-        "Emma Watson",
-        "Will Smith",
-        "Jenna Ortega",
-        "Nina Dobrev",
-        "Candice Accola",
-        "Tania Raymonde",
-        "Chrisopher Masteron",
-        "Noel Fisher",
-        "Emmy Rossum",
-        "Emmy Kenney",
-        "Shanola Hampton",
-        "Eliza Taylor",
-        "Bob Marley",
-        "Marie Avgeropoulos",
-        "Lindsey Morgan",
-        "Thomas McDonell",
-        "Ricky Whittle",
-        "Devon Bostick",
-        "Andrew Garfield",
-        "Chris Wood",
-        "Claire Holt",
-        "Joseph Morgan",
-        "Seth MacFarlane",
-        "Daniel Gilies",
-        "Benjamin Wadsworth",
-        "Phoebe Tonkin",
-        "Lola Flanery",
-        "Nicole Wallace",
-        "Kylie Cantrall",
-        "Vinnie Hacker",
-        "Kendall Jenner",
-        "Hailey Bieber",
-    ]
+    all: []
 }
 
 
 
 var categories = []
 
+if (localStorage.getItem("custom_category")) {
+    Object.keys(JSON.parse(localStorage.getItem("custom_category"))).forEach(function(key) {
+        if (lists[key]) {
+            delete lists[key];
+        }
+        lists[key] = JSON.parse(localStorage.getItem("custom_category"))[key];
+    });
+}
+
 Object.keys(lists).forEach(function(key) {
     categories.push(key.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase()));
+    lists.all = [...new Set(lists.all.concat(lists[key]))];
 });
-
-if (localStorage.getItem('custom_categories')) {
-    let a = JSON.parse(localStorage.getItem('custom_categories'))
-    categories.concat(a)
-}
